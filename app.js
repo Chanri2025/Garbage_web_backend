ccaabae5e878389be268a541844564b2a0185c53
Chanri2025 <aniruddhag.appynitty@gmail.com>
2025-04-09 15:07:29 +0530

    Changing the port number

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
   res.send("SWM API is running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
